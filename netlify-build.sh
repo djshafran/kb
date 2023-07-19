a181c1e9333599c356f5b8097ef4e1d3a2bd60c8
Ivan <djshafran@yandex.ru>
2023-07-19 08:42:32 +0000

    go install --tags extended

--- netlify-build.sh
+++ netlify-build.sh
@@ -4,7 +4,7 @@ printenv | grep -i GO
 
 apk add --no-cache go hugo git make perl
 #git config --global --add safe.directory '/quartz'
-
+go install --tags extended
 go env
 go install github.com/jackyzha0/hugo-obsidian@latest
 
