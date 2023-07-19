0618bba196739d6a41111972defcf0403c41b797
Ivan <djshafran@yandex.ru>
2023-07-19 08:25:52 +0000

    added go env vars

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # echo "[ENV VAR] GOROOT is: $GOROOT"
 printenv | grep -i GO
+
+apk add --no-cache go hugo git make perl
+#git config --global --add safe.directory '/quartz'
+
+go env
+go install github.com/jackyzha0/hugo-obsidian@latest
+
+#PATH="/root/go/bin:$PATH"
+#git clone https://github.com/djshafran/kb /quartz
+
+#WORKDIR /quartz
+
 make help 
-make update-force
+# make update-force
 make serve
