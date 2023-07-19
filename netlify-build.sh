23d2607fb9255645735a03ed98affb8094115db3
Ivan <djshafran@yandex.ru>
2023-07-19 07:21:31 +0000

    add installing hugo to build

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-docker build -t kb:0.9 . && docker run -dp 1313:1313 -t kb:0.9
+mkdir $HOME/src
+cd $HOME/src
+git clone https://github.com/gohugoio/hugo.git
+cd hugo
+go install --tags extended
+cd ../
